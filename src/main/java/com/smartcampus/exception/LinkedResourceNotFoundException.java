4982a27eb5bc513886acbf7a7c305b7fa1c9fd52
Avishka Madushan <avishkamadushan4338@gmail.com>
2026-04-23 21:48:20 +0530

    Fix javadoc: LinkedResourceNotFoundException maps to 422 not 404

--- src/main/java/com/smartcampus/exception/LinkedResourceNotFoundException.java
+++ src/main/java/com/smartcampus/exception/LinkedResourceNotFoundException.java
@@ -3,7 +3,7 @@ package com.smartcampus.exception;
 /**
  * Thrown when a resource references a linked resource that does not exist.
  * For example, creating a sensor with a roomId that has not been registered.
- * Mapped to HTTP 404 Not Found by LinkedResourceNotFoundExceptionMapper.
+ * Mapped to HTTP 422 Unprocessable Entity by LinkedResourceNotFoundExceptionMapper.
  */
 public class LinkedResourceNotFoundException extends RuntimeException {
 
